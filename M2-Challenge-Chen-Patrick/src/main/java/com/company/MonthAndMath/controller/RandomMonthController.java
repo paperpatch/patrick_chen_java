3b5aea61896fea8afa07241245c02c3a78d1f68c
paperpatch <patchen21@gmail.com>
2022-07-06 23:02:52 -0400

    added random month controller and model

--- M2-Challenge-Chen-Patrick/src/main/java/com/company/MonthAndMath/controller/RandomMonthController.java
+++ M2-Challenge-Chen-Patrick/src/main/java/com/company/MonthAndMath/controller/RandomMonthController.java
@@ -1,4 +1,44 @@
 package com.company.MonthAndMath.controller;
 
+import com.company.MonthAndMath.models.RandomMonth;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+@RestController
 public class RandomMonthController {
+    private static int idCounter = 1;
+
+    private static List<RandomMonth> monthList = new ArrayList<>(Arrays.asList(
+            new RandomMonth("January", idCounter++),
+            new RandomMonth("February", idCounter++),
+            new RandomMonth("March", idCounter++),
+            new RandomMonth("April", idCounter++),
+            new RandomMonth("May", idCounter++),
+            new RandomMonth("June", idCounter++),
+            new RandomMonth("July", idCounter++),
+            new RandomMonth("August", idCounter++),
+            new RandomMonth("September", idCounter++),
+            new RandomMonth("October", idCounter++),
+            new RandomMonth("November", idCounter++),
+            new RandomMonth("December", idCounter++)
+    ));
+
+    @RequestMapping(value="/randomMonth", method= RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public RandomMonth getRandomResponse() {
+        Random randomNumberGenerator = new Random();
+
+        int randomNumber = randomNumberGenerator.nextInt(monthList.size());
+
+        return monthList.get(randomNumber);
+    }
+
 }
--- M2-Challenge-Chen-Patrick/src/main/java/com/company/MonthAndMath/models/RandomMonth.java
+++ M2-Challenge-Chen-Patrick/src/main/java/com/company/MonthAndMath/models/RandomMonth.java
@@ -1,4 +1,50 @@
 package com.company.MonthAndMath.models;
 
+import java.util.Objects;
+
 public class RandomMonth {
+    private int id;
+    private String month;
+
+    public RandomMonth(String month, int id) {
+        this.id = id;
+        this.month = month;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RandomMonth that = (RandomMonth) o;
+        return id == that.id && Objects.equals(month, that.month);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, month);
+    }
+
+    @Override
+    public String toString() {
+        return "RandomMonth{" +
+                "id=" + id +
+                ", month='" + month + '\'' +
+                '}';
+    }
 }
