e320f8160fab462f887787ed65cd3ab159dcacd0
paperpatch <patchen21@gmail.com>
2022-07-06 22:39:16 -0400

    added Month Converter controller. deleted model. added test layout

--- M2-Challenge-Chen-Patrick/src/main/java/com/company/MonthAndMath/controller/MonthConverterController.java
+++ M2-Challenge-Chen-Patrick/src/main/java/com/company/MonthAndMath/controller/MonthConverterController.java
@@ -1,25 +1,43 @@
 package com.company.MonthAndMath.controller;
 
-import com.company.MonthAndMath.models.MonthConverter;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.lang.reflect.Array;
-
+// originally written in the Cognizant 05-stu-month-converter
 @RestController
 public class MonthConverterController {
-    private int monthNumber;
-
-    private static String[] months = {
-            "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
-    };
-
     @RequestMapping(value="/month/{monthNumber}", method= RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
-    public MonthConverter getConvertedMonth() {
-        return monthNumber;
+    public String getMonth(@PathVariable String monthNumber) {
+
+        switch (monthNumber) {
+            case "1":
+                return "January";
+            case "2":
+                return "February";
+            case "3":
+                return "March";
+            case "4":
+                return "April";
+            case "5":
+                return "May";
+            case "6":
+                return "June";
+            case "7":
+                return "July";
+            case "8":
+                return "August";
+            case "9":
+                return "September";
+            case "10":
+                return "October";
+            case "11":
+                return "November";
+            case "12":
+                return "December";
+            default:
+                throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "Invalid Month Number Specified");
+        }
     }
 }
