4bd641ed3dd4018209f6f3ba5239c5fcb208f9a2
paperpatch <patchen21@gmail.com>
2022-07-06 22:46:20 -0400

    completed month converter tests

--- M2-Challenge-Chen-Patrick/src/test/java/com/company/MonthAndMath/controller/MonthConverterControllerTest.java
+++ M2-Challenge-Chen-Patrick/src/test/java/com/company/MonthAndMath/controller/MonthConverterControllerTest.java
@@ -2,16 +2,49 @@ package com.company.MonthAndMath.controller;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+// originally written in the Cognizant 05-stu-month-converter
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(MonthConverterController.class)
 public class MonthConverterControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void shouldReturnMonthOnValidGetRequest() throws Exception {
+        String outputJson = "August";
+
+        mockMvc.perform(
+                        get("/month/8")
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(outputJson));
+    }
+
+    @Test
+    public void shouldReturn422ForInvalidMonthId() throws Exception {
+        mockMvc.perform(
+                        get("/month/999")
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
     }
 }
